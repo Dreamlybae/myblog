9772844ac4f8558571cd3c07869217d2eda30b5a
Dreamlybae <azat-zakirov-04@mail.ru>
2023-08-03 14:14:57 +0300

    adel123

---
+++ resources/views/adelfenin.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Адель Чикибряков</title>
+        <style>
+            body{
+                background: lightblue;
+            }
+            #header{
+                /* background: red; */
+                height: 100px;
+                width: 100%;
+                text-align: center;
+                font-size: 100px;
+            }
+            #img{
+                /* background: pink; */
+                height: 500px;
+                width: 100%;
+                text-align: center;
+            }
+            #content{
+                /* background: blue; */
+                height: 300px;
+                width: 100%;
+                text-align: center;
+                font-size: 60px;
+            }
+        </style>
+    </head>
+    <body>
+        <div id="header">Адель Чикибряков</div>
+        <div id="img"><img src="{{ asset('images/adelfenin.jpg') }}" alt="description of myimage" width=1000px height=400px></div>
+        <div id="content">Адель Чикибряков празднует свое 13-ти летие. Сегодня Адель стал на шаг ближе к получению паспорта.
+             В этот день ровно год назад Адель Чикибряков впервые попробовал сигарету, а сейчас уже месяц не курит. Воспитательная беседа Олега дала свои плоды.</div>
+    </body>
+</html>
